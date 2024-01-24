5a95453ea9f238b10c2915b441d90d74bdaa6c5e
n4n44 <n4n44nanashi@gmail.com>
2024-01-24 09:10:43 +0900

    区間推定の追加

--- test_tr_estmtr.py
+++ test_tr_estmtr.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tr_estmtr import tr_estmtr, interval_estmt
 
 dim = 100
 
@@ -10,19 +11,25 @@ print(B)
 A = B@B.T
 
 print(A)
-print(f"trace : {np.trace(A)}")
+tr_a = np.trace(A)
+print(f"trace : {tr_a}")
 
-w = np.random.standard_normal((30,dim))
+# w = np.random.standard_normal((300,dim))
 sum = 0
 data = []
 for i in range(1000):
-    x = tr_estmtr(A)
-    data = np.append(data,x)
+    x_bar, lo_bnd, up_bnd = interval_estmt(A, 10, 0.05)
+    conf_intvl=(lo_bnd, up_bnd)
+    res =  (conf_intvl[0] <= tr_a and conf_intvl[1] >= tr_a)
+    print(f"{conf_intvl} : {res}")
+    data = np.append(data,res)
 
-plt.hist(data, bins=30, color='blue', alpha=0.7)
+correct = np.sum(data)/len(data)*100
+print(f"正解率{correct}%")
+# plt.hist(data, bins=30, color='blue', alpha=0.7)
 
-plt.title('Histogram')
-plt.xlabel('Value')
-plt.ylabel('Frequency')
+# plt.title('Histogram')
+# plt.xlabel('Value')
+# plt.ylabel('Frequency')
 
-plt.show()
+# plt.show()
--- tr_estmtr/tr_estmtr.py
+++ tr_estmtr/tr_estmtr.py
@@ -1,16 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-def sampling(A,sample_size=30):# 標本からtrace estimatorを計算
+from scipy.stats import t
+def sampling(A,sample_size=30 , dist = np.random.standard_normal):# 標本からtrace estimatorを計算
     dim = A.shape[0]
     x = []
-    w = np.random.standard_normal((sample_size,dim))
+    w = dist((sample_size,dim))
     for i in range(1,sample_size):
         x_k = w[i]@A@w[i].T
         x = np.append(x,x_k)
     return x
 
-def tr_estmtr(A, sample_size=30):# trace estimatorの標本平均を計算
-    ret = sampling(A, sample_size)
+def tr_estmtr(A, sample_size=30, dist = np.random.standard_normal):# trace estimatorの標本平均を計算
+    ret = sampling(A, sample_size, dist)
     x_bar = np.sum(ret)/sample_size
-    return x_bar
+    s_bar = 0
+    for x in ret:
+        s_bar+=(x-x_bar)**2
+    s_bar/=(sample_size-1)
+    return x_bar,s_bar
+
+def interval_estmt(A, sample_size=30, alpha = 0.05):
+    x_bar, s_bar = tr_estmtr(A, sample_size)
+    t_val = t.ppf(1-alpha,sample_size-1)
+    return x_bar,x_bar-t_val*np.sqrt(s_bar),x_bar+t_val*np.sqrt(s_bar)
+    
